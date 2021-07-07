551262521e5b61c2b0a05c6ad261426918deafa0
landon hickman <landonhickman12@gmail.com>
2021-07-07 10:15:09 -0600

    api call to heroku

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useState, useEffect} from 'react'
 
 const Home = () => {
+  const [things, setThings]=useState([])
+
+  useEffect(()=>{
+    getThings()
+  },[])
+
+  const getThings = async () => {
+    try{
+      let res = await axios.get('https://heroku-sum21.herokuapp.com/api/things')
+      console.log(res)
+      setThings(res.data)
+    }catch(err){
+      console.log(err)
+    }
+  }
   return (
     <div>
       <h1>Home</h1>
+      {things && things.length === 0 && <p>0 things</p>}
+      {things && things.map(t=> (
+      <p>{t.name}</p>))}
     </div>
   )
 }
